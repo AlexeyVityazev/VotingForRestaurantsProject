52e941771b765189568ffa6be329baf6054e9fe3
AlexeyVityazev1987 <alexeyvityazev@gmail.com>
2023-05-18 17:57:13 +0300

    Add changes to Swagger

--- src/main/java/ru/vityazev/voting/config/OpenApiConfig.java
+++ src/main/java/ru/vityazev/voting/config/OpenApiConfig.java
@@ -22,13 +22,19 @@ import org.springframework.context.annotation.Configuration;
                 title = "REST API documentation",
                 version = "1.0",
                 description = """
-                        Приложение по <a href='https://javaops.ru/view/topjava2'>курсу TopJava-2</a>: решение выпускного проекта
+                        Приложение для голосования за рестораны <br>
+                        <br>                        
+                        Сценарий:<br>
+                                                
+                        0. Администратор добавляет меню для ресторанов на завтра //TODO in development <br>
+                        1. Пользователь смотрит на имеющиеся меню у ресторанов на сегодня <a href="#/dish-restaurant-controller/getAll_3">GET /api/admin/dish</a> <br>
+                        2. Пользователь голосует за понравившийся ресторан <a href="#/user-voting-controller/voteForRestaurant">POST /api/users/vote/{id}</a> <br>
+                        <br>
                         <p><b>Тестовые креденшелы:</b><br>
                         - user@yandex.ru / password<br>
                         - admin@gmail.com / admin<br>
                         - guest@gmail.com / guest</p>
-                        """,
-                contact = @Contact(url = "https://javaops.ru/#contacts", name = "Grigory Kislin", email = "admin@javaops.ru")
+                        """
         ),
         security = @SecurityRequirement(name = "basicAuth")
 )
